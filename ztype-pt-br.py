2423300739ace9456a6c56746e4768538833a2ea
Thiago <thiokue@gmail.com>
2022-11-27 01:07:29 -0300

    1.0

--- ztype-pt-br.py
+++ ztype-pt-br.py
@@ -4,8 +4,7 @@ import random
 dicionario = {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": [], "I": [], "J": [], "K": [], "L": [], "M": [], "N": [], "O": [], "P": [], "Q": [], "R": [], "S": [], "T": [], "U": [], "V": [], "W": [], "X": [], "Y": [], "Z": []}
 c = 0
 alfabeto = list(dicionario.keys())
-word = {}
-level = []
+string = ""
 
 df = pandas.read_csv("br-sem-acentos.txt")
 for i in df["a"]:
@@ -13,15 +12,11 @@ for i in df["a"]:
         dicionario[alfabeto[c]].append(i)
     else:
         c += 1
-        
-for x in range(0, 50):
+for i in range(0, 10): 
     for i in alfabeto:
         if len(dicionario[i]) > 0:
-            word = {x : (dicionario[i][(random.randint(0, (len(dicionario[i])-1)))])}
+            string += (dicionario[i][(random.randint(0, (len(dicionario[i])-1)))]) + " "
 
-
-
-
-print(word)
+print(string)
 
 
